af3df99f33cc6c5061b0551ac8ebbc709895d9bb
nitao <nitao@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-01-02 17:23:55 +0000

    Movendo os projetos antigos para a nova estrutura de árvore. Hugo

---
+++ br.org.archimedes.orto.tests/trunk/br.org.archimedes.orto.tests/test/br/org/archimedes/orto/OrtoTest.java
@@ -0,0 +1,72 @@
+/*
+ * Created on 22/08/2006
+ */
+
+package br.org.archimedes.orto;
+
+import junit.framework.TestCase;
+import br.org.archimedes.gui.model.Workspace;
+import br.org.archimedes.interfaces.Command;
+import br.org.archimedes.model.Drawing;
+
+
+/**
+ * Belongs to package com.tarantulus.archimedes.model.commands.
+ * 
+ * @author night
+ */
+public class OrtoTest extends TestCase {
+
+    /*
+     * Test method for
+     * 'com.tarantulus.archimedes.model.commands.OrtoCommand.doIt(Drawing)'
+     */
+    public void testDoIt () {
+
+        Drawing drawing = new Drawing("Drawing");
+
+        Workspace workspace = Workspace.getInstance();
+        boolean orto = workspace.isOrtoOn();
+
+        Command ortoCommand = new OrtoCommand();
+
+        try {
+            ortoCommand.doIt(null);
+        }
+        catch (Exception e) {
+            fail("Should not throw any Exception.");
+        }
+        assertEquals("The orto state should have changed.", !orto, workspace
+                .isOrtoOn());
+        
+        try {
+            ortoCommand.doIt(drawing);
+        }
+        catch (Exception e) {
+            fail("Should not throw any exception");
+        }
+
+        assertEquals("The orto state should have changed.", orto, workspace
+                .isOrtoOn());
+
+        try {
+            ortoCommand.doIt(drawing);
+        }
+        catch (Exception e) {
+            fail("Should not throw any exception");
+        }
+
+        assertEquals("The orto state should have changed.", !orto, workspace
+                .isOrtoOn());
+        
+        try {
+            ortoCommand.doIt(drawing);
+        }
+        catch (Exception e) {
+            fail("Should not throw any exception");
+        }
+
+        assertEquals("The orto state should have changed.", orto, workspace
+                .isOrtoOn());
+    }
+}
