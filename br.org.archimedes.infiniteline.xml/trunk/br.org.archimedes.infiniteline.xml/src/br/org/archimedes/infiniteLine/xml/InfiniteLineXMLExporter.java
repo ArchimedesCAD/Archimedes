f15676244173d43a1242bd214da592d86ab13418
nitao <nitao@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-01-02 21:55:14 +0000

    Movendo os projetos antigos para a nova estrutura de árvore. Hugo

---
+++ br.org.archimedes.infiniteline.xml/trunk/br.org.archimedes.infiniteline.xml/src/br/org/archimedes/infiniteLine/xml/InfiniteLineXMLExporter.java
@@ -0,0 +1,37 @@
+package br.org.archimedes.infiniteLine.xml;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import br.org.archimedes.infiniteline.InfiniteLine;
+import br.org.archimedes.interfaces.ElementExporter;
+import br.org.archimedes.io.xml.XMLExporterHelper;
+
+/**
+ * Exporter for InfiniteLine.
+ * 
+ * @author werpinheiro
+ */
+public class InfiniteLineXMLExporter implements ElementExporter<InfiniteLine> {
+
+	/**
+	 * (non-Javadoc).
+	 * 
+	 * @see br.org.archimedes.interfaces.ElementExporter#exportElement(br.org.archimedes.model.Element,
+	 *      java.io.OutputStream)
+	 */
+	public void exportElement(InfiniteLine element, OutputStream output)
+			throws IOException {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<infiniteLine>"); //$NON-NLS-1$
+
+		sb.append(XMLExporterHelper.xmlFor("point", element.getInitialPoint())); //$NON-NLS-1$
+		sb.append(XMLExporterHelper.xmlFor("point", element.getEndingPoint())); //$NON-NLS-1$
+		
+		sb.append("</infiniteLine>"); //$NON-NLS-1$
+
+		output.write(sb.toString().getBytes());
+	}
+
+}
