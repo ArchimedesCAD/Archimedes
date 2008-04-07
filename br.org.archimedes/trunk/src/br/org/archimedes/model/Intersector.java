f89bc97c81d75a8a8121d5950b884d1858ae292a
lreal <lreal@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-04-07 14:36:55 +0000

    Modified the Intersector interface to include a NullArgumentException throwing declaration

--- br.org.archimedes/trunk/src/br/org/archimedes/model/Intersector.java
+++ br.org.archimedes/trunk/src/br/org/archimedes/model/Intersector.java
@@ -2,6 +2,8 @@ package br.org.archimedes.model;
 
 import java.util.Collection;
 
+import br.org.archimedes.exceptions.NullArgumentException;
+
 public interface Intersector {
 	
 	/**
@@ -12,8 +14,9 @@ public interface Intersector {
      * @param otherElement
      *            The second element.
      * @return The collection of points of intersection.
+	 * @throws NullArgumentException If element or otherElement is null
      */
 	
-	public Collection<Point> getIntersections(Element element, Element otherElement);
+	public Collection<Point> getIntersections(Element element, Element otherElement) throws NullArgumentException;
 	
 }
