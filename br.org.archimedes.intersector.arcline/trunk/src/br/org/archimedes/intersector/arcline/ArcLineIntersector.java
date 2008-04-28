8042a58f37e4d7ca90e8e9f82fb3caed6ddf4db2
keizo <keizo@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-04-28 14:09:43 +0000

    Arc Line Intersector creation.

---
+++ br.org.archimedes.intersector.arcline/trunk/src/br/org/archimedes/intersector/arcline/ArcLineIntersector.java
@@ -0,0 +1,54 @@
+package br.org.archimedes.intersector.arcline;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import br.org.archimedes.arc.Arc;
+import br.org.archimedes.circle.Circle;
+import br.org.archimedes.exceptions.InvalidArgumentException;
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.intersector.circleline.CircleLineIntersector;
+import br.org.archimedes.line.Line;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Intersector;
+import br.org.archimedes.model.Point;
+
+public class ArcLineIntersector implements Intersector {
+
+	public Collection<Point> getIntersections(Element element,
+			Element otherElement) throws NullArgumentException {
+		
+		CircleLineIntersector intersector = new CircleLineIntersector();
+		Arc arc = null;
+		Line line = null;
+		Circle circle = null;
+		Collection<Point> intersections = null;
+		
+		if (element.getClass() == Line.class) {
+			line = (Line) element;
+			arc = (Arc) otherElement;
+		} else {
+			line = (Line) otherElement;
+			arc = (Arc) element;
+		}
+		
+		try {
+			circle = new Circle(arc.getCenter(), arc.getRadius());
+		} catch (InvalidArgumentException e) {
+			e.printStackTrace();
+		}
+		
+		intersections = intersector.getIntersections(circle, line);
+		
+		Collection<Point> arcIntersections = new ArrayList<Point>();
+		
+		for(Point p : intersections){
+			if(arc.contains(p)){
+				arcIntersections.add(p);
+			}
+		}
+		
+		return arcIntersections;
+	}
+
+}
