6008382dd93ed752b6b94ee5aa76d0a1da662281
lreal <lreal@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-05-12 14:06:16 +0000

    Arc Polyline Intersector.

---
+++ br.org.archimedes.intersector.arcpolyline/trunk/src/br/org/archimedes/intersector/arcpolyline/ArcPolylineIntersector.java
@@ -0,0 +1,48 @@
+package br.org.archimedes.intersector.arcpolyline;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import br.org.archimedes.arc.Arc;
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.interfaces.Intersector;
+import br.org.archimedes.intersector.arcline.ArcLineIntersector;
+import br.org.archimedes.line.Line;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Point;
+import br.org.archimedes.polyline.Polyline;
+
+public class ArcPolylineIntersector implements Intersector {
+
+	@Override
+	public Collection<Point> getIntersections(Element element,
+			Element otherElement) throws NullArgumentException {
+		Arc baseArc;
+		Polyline polyline;
+
+		if (element == null || otherElement == null)
+			throw new NullArgumentException();
+
+		if (element.getClass() == Arc.class) {
+			baseArc = (Arc) element;
+			polyline = (Polyline) otherElement;
+		} else {
+			baseArc = (Arc) otherElement;
+			polyline = (Polyline) element;
+		}
+
+		List<Line> lines = polyline.getLines();
+		Collection<Point> intersectionPoints = new ArrayList<Point>();
+
+		Collection<Point> intersection;
+
+		ArcLineIntersector arcLineIntersector = new ArcLineIntersector();
+		for (Line line : lines) {
+			intersection = arcLineIntersector.getIntersections(line, baseArc);
+			intersectionPoints.addAll(intersection);
+		}
+		return intersectionPoints;
+	}
+
+}
