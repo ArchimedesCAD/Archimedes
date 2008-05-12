15cb80365ef1b6b09eac3c0c8b859e43a0779c9d
lreal <lreal@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-05-12 16:10:21 +0000

    Polyline Polyline intersector.

---
+++ br.org.archimedes.intersector.polylinepolyline/trunk/src/br/org/archimedes/intersector/polylinepolyline/PolylinePolylineIntersector.java
@@ -0,0 +1,19 @@
+package br.org.archimedes.intersector.polylinepolyline;
+
+import java.util.Collection;
+
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.interfaces.Intersector;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Point;
+
+public class PolylinePolylineIntersector implements Intersector {
+
+	@Override
+	public Collection<Point> getIntersections(Element element,
+			Element otherElement) throws NullArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
