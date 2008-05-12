b178f7ee3fbd3b6453fb1f6da8f383fdcafa2421
lreal <lreal@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-05-12 16:12:39 +0000

    Tests for Polyline Polyline intersector. Incomplete.

---
+++ br.org.archimedes.intersector.polylinepolyline.tests/trunk/src/br/org/archimedes/intersector/polylinepolyline/tests/PolylinePolylineIntersectorTest.java
@@ -0,0 +1,74 @@
+package br.org.archimedes.intersector.polylinepolyline.tests;
+
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.org.archimedes.Tester;
+import br.org.archimedes.exceptions.InvalidArgumentException;
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.interfaces.Intersector;
+import br.org.archimedes.intersector.polylinepolyline.PolylinePolylineIntersector;
+import br.org.archimedes.model.Point;
+import br.org.archimedes.polyline.Polyline;
+
+public class PolylinePolylineIntersectorTest extends Tester {
+	Polyline polyline;
+	Intersector intersector;
+
+	@Before
+	public void setUp() throws NullArgumentException, InvalidArgumentException {
+		ArrayList<Point> polyPoints = new ArrayList<Point>();
+		polyPoints.add(new Point(-1.0, 1.0));
+		polyPoints.add(new Point(1.0, 1.0));
+		polyPoints.add(new Point(-1.0, -1.0));
+		polyPoints.add(new Point(1.0, -1.0));
+		polyline = new Polyline(polyPoints);
+		intersector = new PolylinePolylineIntersector();
+	}
+
+	@Test
+	public void nullArgumentsShouldRaiseException()
+			throws NullArgumentException, InvalidArgumentException {
+		try {
+			intersector.getIntersections(polyline, null);
+			fail("The otherElement is null and getIntersections should have thrown a NullArgumentException");
+		} catch (NullArgumentException e) {
+			// Passed
+		}
+
+		try {
+			intersector.getIntersections(null, polyline);
+			fail("The element is null and getIntersections should have thrown a NullArgumentException");
+		} catch (NullArgumentException e) {
+			// Passed
+		}
+
+		try {
+			intersector.getIntersections(null, null);
+			fail("Both elements are null and getIntersections should have thrown a NullArgumentException");
+		} catch (NullArgumentException e) {
+			// Passed
+		}
+		Assert.assertTrue("Raised all exceptions", true);
+	}
+
+	@Test
+	public void polylineNotIntersectingPolylineReturnsNoIntersections()
+			throws NullArgumentException, InvalidArgumentException {
+		List<Point> polyPoints = new ArrayList<Point>();
+		polyPoints.add(new Point(-2.0, -2.0));
+		polyPoints.add(new Point(2.0, 2.0));
+		polyPoints.add(new Point(0.0, 2.0));
+		Polyline testPoly = new Polyline(polyPoints);
+
+		assertCollectionTheSame(Collections.EMPTY_LIST, intersector
+				.getIntersections(testPoly, polyline));
+	}
+}
