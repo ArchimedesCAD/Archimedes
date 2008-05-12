7866c33f048a6fbaf6b6f6b02fb6001e7fb98931
keizo <keizo@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-05-12 20:35:34 +0000

    The Arc-Circle Intersector.

---
+++ br.org.archimedes.intersector.arccircle/trunk/src/br/org/archimedes/intersector/arccircle/ArcCircleIntersector.java
@@ -0,0 +1,56 @@
+package br.org.archimedes.intersector.arccircle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import br.org.archimedes.arc.Arc;
+import br.org.archimedes.circle.Circle;
+import br.org.archimedes.exceptions.InvalidArgumentException;
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.interfaces.Intersector;
+import br.org.archimedes.intersector.circlecircle.CircleCircleIntersector;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Point;
+
+public class ArcCircleIntersector implements Intersector{
+
+	public Collection<Point> getIntersections(Element element,
+			Element otherElement) throws NullArgumentException {
+		Arc baseArc;
+		Circle circle;
+
+		if (element == null || otherElement == null)
+			throw new NullArgumentException();
+
+		if (element.getClass() == Arc.class) {
+			baseArc = (Arc) element;
+			circle = (Circle) otherElement;
+		} else {
+			baseArc = (Arc) otherElement;
+			circle = (Circle) element;
+		}
+		
+		Circle fakeCircle = null;
+		try {
+			fakeCircle = new Circle(baseArc.getCenter(), baseArc.getRadius());
+		} catch (InvalidArgumentException e) {
+			e.printStackTrace();
+		}
+		
+		CircleCircleIntersector cci = new CircleCircleIntersector();
+		Collection<Point> intersectionPoints = cci.getIntersections(circle, fakeCircle);
+		Collection<Point> arcIntersections = new ArrayList<Point>();
+		
+		for(Point p : intersectionPoints){
+			if(baseArc.contains(p)){
+				arcIntersections.add(p);
+			}
+		}
+		
+		return arcIntersections;
+
+
+		
+	}
+
+}
