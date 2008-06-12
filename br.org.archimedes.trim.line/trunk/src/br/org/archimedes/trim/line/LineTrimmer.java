d9ecd6121363aaebce94729bb9775910a469be44
keizo <keizo@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-06-12 13:05:10 +0000

    Initial implementation of a Line Trim.

---
+++ br.org.archimedes.trim.line/trunk/src/br/org/archimedes/trim/line/LineTrimmer.java
@@ -0,0 +1,17 @@
+package br.org.archimedes.trim.line;
+
+import java.util.Collection;
+
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.trims.interfaces.Trimmer;
+
+public class LineTrimmer implements Trimmer {
+
+	public Collection<Element> trim(Element element,
+			Collection<Element> references) throws NullArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
