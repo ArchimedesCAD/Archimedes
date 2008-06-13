930d3ba68aa3e6660af7bbf45c4e2f424e13607a
lreal <lreal@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-06-13 14:05:40 +0000

    Adding the trim manager to the Core (registering it in Utils). Jonas & Luiz

--- br.org.archimedes/trunk/src/br/org/archimedes/trims/NullTrimManager.java
+++ br.org.archimedes/trunk/src/br/org/archimedes/trims/NullTrimManager.java
@@ -1,24 +1,15 @@
-/*
- * Created on June 9, 2008 for br.org.archimedes
- */
-
 package br.org.archimedes.trims;
 
 import java.util.Collection;
 import java.util.Collections;
 
 import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.interfaces.TrimManager;
 import br.org.archimedes.model.Element;
-import br.org.archimedes.trims.interfaces.Trimmer;
 
-/**
- * Belongs to package br.org.archimedes.trims.
- * 
- * @author ceci
- */
-public class NullTrimmer implements Trimmer {
+public class NullTrimManager implements TrimManager {
 
-	public Collection<Element> trim(Element element,
+	public Collection<Element> getTrimOf(Element element,
 			Collection<Element> references) throws NullArgumentException {
 		
 		return Collections.emptyList();
