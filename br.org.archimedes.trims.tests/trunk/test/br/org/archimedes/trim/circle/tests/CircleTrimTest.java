4ed6c12158daf2b29e65549c519628dc45772e04
lreal <lreal@a010ea30-1657-0410-bcc9-f6899aecc918>
2008-06-26 12:21:59 +0000

    Adding the files missing on the last commit :)

---
+++ br.org.archimedes.trims.tests/trunk/test/br/org/archimedes/trim/circle/tests/CircleTrimTest.java
@@ -0,0 +1,77 @@
+package br.org.archimedes.trim.circle.tests;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.org.archimedes.Tester;
+import br.org.archimedes.arc.Arc;
+import br.org.archimedes.circle.Circle;
+import br.org.archimedes.exceptions.InvalidArgumentException;
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.line.Line;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Point;
+import br.org.archimedes.trim.circle.CircleTrimmer;
+import br.org.archimedes.trims.interfaces.Trimmer;
+
+public class CircleTrimTest extends Tester {
+
+	private Circle testCircle;
+	private Trimmer trimmer;
+	
+	@Before
+	public void setUp() throws NullArgumentException, InvalidArgumentException {
+		testCircle = new Circle(new Point(0.0, 0.0), 1.0);
+		trimmer = new CircleTrimmer();
+	}
+	
+	@Test
+	public void tangentLineCannotTrimCircle() throws NullArgumentException, InvalidArgumentException {
+		Element line = new Line(new Point(1.0, 1.0), new Point(1.0, -1.0));
+		Collection<Element> trimmed = trimmer.trim(testCircle, Collections.singleton(line), new Point(0.0, 1.0));
+		assertCollectionTheSame(Collections.singleton(testCircle), trimmed);
+	}
+	
+	@Test
+	public void lineIntersectsCircleOnceCannotTrimCircle() throws NullArgumentException, InvalidArgumentException {
+		Element line = new Line(new Point(0.0, 2.0), new Point(0.0, 0.5));
+		Collection<Element> trimmed = trimmer.trim(testCircle, Collections.singleton(line), new Point(1.0, 0.0));
+		assertCollectionTheSame(Collections.singleton(testCircle), trimmed);
+	}
+	
+	@Test
+	public void secantLineTrimsCircleResultsHalfCircle() throws NullArgumentException, InvalidArgumentException {
+		Element line = new Line(new Point(0.0, 2.0), new Point(0.0, -2.0));
+		Collection<Element> trimmed = trimmer.trim(testCircle, Collections.singleton(line), new Point(1.0, 0.0));
+		assertCollectionTheSame(Collections.singleton(new Arc(new Point(0.0, 1.0), new Point(-1.0, 0.0), new Point(0.0, -1.0))), trimmed);
+	}
+	
+	@Test
+	public void twoSecantLinesTrimsCircleResultsThreeQuartersOfCircle() throws NullArgumentException, InvalidArgumentException {
+		Element line1 = new Line(new Point(0.0, 2.0), new Point(0.0, -2.0));
+		Element line2 = new Line(new Point(-2.0, 0.0), new Point(2.0, 0.0));
+		Collection<Element> references = new ArrayList<Element>();
+		references.add(line1);
+		references.add(line2);
+		Collection<Element> trimmed = trimmer.trim(testCircle, references, new Point(Math.sqrt(2.0) / 2.0, Math.sqrt(2.0) / 2.0));
+		assertCollectionTheSame(Collections.singleton(new Arc(new Point(0.0, 1.0), new Point(-1.0, 0.0), new Point(1.0, 0.0))), trimmed);
+	}
+	
+	@Test
+	public void secantLineTrimCircleResultsLeftHalfOfCircleWhenClickedInIntersection() throws NullArgumentException, InvalidArgumentException {
+		Element line = new Line(new Point(0.0, 2.0), new Point(0.0, -2.0));
+		Collection<Element> trimmed = trimmer.trim(testCircle, Collections.singleton(line), new Point(0.0, 1.0));
+		assertCollectionTheSame(Collections.singleton(new Arc(new Point(0.0, 1.0), new Point(-1.0, 0.0), new Point(0.0, -1.0))), trimmed);
+	}
+	
+	@Test
+	public void referencesDontIntersectCircleDontTrim() throws NullArgumentException, InvalidArgumentException {
+		Element line = new Line(new Point(2.0, 2.0), new Point(2.0, -2.0));
+		Collection<Element> trimmed = trimmer.trim(testCircle, Collections.singleton(line), new Point(1.0, 0.0));
+		assertCollectionTheSame(Collections.singleton(testCircle), trimmed);
+	}
+}
---
+++ br.org.archimedes.trims/trunk/src/br/org/archimedes/trim/circle/CircleTrimmer.java
@@ -0,0 +1,131 @@
+package br.org.archimedes.trim.circle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import br.org.archimedes.Geometrics;
+import br.org.archimedes.arc.Arc;
+import br.org.archimedes.circle.Circle;
+import br.org.archimedes.exceptions.InvalidArgumentException;
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.interfaces.IntersectionManager;
+import br.org.archimedes.model.ComparablePoint;
+import br.org.archimedes.model.DoubleKey;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Point;
+import br.org.archimedes.rcp.extensionpoints.IntersectionManagerEPLoader;
+import br.org.archimedes.trims.interfaces.Trimmer;
+
+public class CircleTrimmer implements Trimmer {
+
+	private IntersectionManager intersectionManager;
+
+	public CircleTrimmer() {
+		intersectionManager = new IntersectionManagerEPLoader()
+		.getIntersectionManager();
+	}
+	
+	public Collection<Element> trim(Element element,
+			Collection<Element> references, Point click)
+			throws NullArgumentException {
+		
+		if (element == null || references == null || click == null) {
+			throw new NullArgumentException();
+		}
+		Circle circle = (Circle) element;
+				
+		Collection<Element> trimResult = new ArrayList<Element>();
+        Collection<Point> intersectionPoints = intersectionManager.getIntersectionsBetween(element, references);
+        
+        if (intersectionPoints.size() <= 1)
+        	return Collections.singleton(element);
+        
+        Point initialPoint = circle.getCenter().clone();
+        initialPoint.move(circle.getRadius(), 0);
+        SortedSet<ComparablePoint> sortedPointSet = getSortedPointSet(circle,
+                initialPoint, intersectionPoints);
+
+        ComparablePoint clickPoint = null;
+
+        try {
+            double key = Geometrics.calculateRelativeAngle(circle.getCenter(),
+                    initialPoint, click);
+            clickPoint = new ComparablePoint(click, new DoubleKey(key));
+        }
+        catch (NullArgumentException e) {
+            // Should never reach
+            e.printStackTrace();
+        }
+
+        SortedSet<ComparablePoint> negativeIntersections = sortedPointSet.headSet(clickPoint);
+        SortedSet<ComparablePoint> positiveIntersections = sortedPointSet.tailSet(clickPoint);
+
+        try {
+            if (negativeIntersections.size() == 0
+                    && positiveIntersections.size() > 0) {
+                Point firstPositive = positiveIntersections.first().getPoint();
+                Point lastPositive = positiveIntersections.last().getPoint();
+                Element arc = new Arc(firstPositive, lastPositive, circle.getCenter(), true);
+                arc.setLayer(circle.getLayer());
+
+                trimResult.add(arc);
+            }
+            else if (positiveIntersections.size() == 0
+                    && negativeIntersections.size() > 0) {
+                Point lastNegative = negativeIntersections.last().getPoint();
+                Point firstNegative = negativeIntersections.first().getPoint();
+                Element arc = new Arc(firstNegative, lastNegative, circle.getCenter(), true);
+                arc.setLayer(circle.getLayer());
+
+                trimResult.add(arc);
+            }
+            else if (negativeIntersections.size() > 0
+                    && positiveIntersections.size() > 0) {
+                Point firstPositive = positiveIntersections.first().getPoint();
+                Point lastNegative = negativeIntersections.last().getPoint();
+                Element arc = new Arc(firstPositive, lastNegative, circle.getCenter(), true);
+                arc.setLayer(circle.getLayer());
+
+                trimResult.add(arc);
+            }
+        }
+        catch (NullArgumentException e) {
+            // Should not catch this exception
+            e.printStackTrace();
+        }
+        catch (InvalidArgumentException e) {
+            // Should not catch this exception
+            e.printStackTrace();
+        }
+
+        return trimResult;
+
+	}
+
+	private SortedSet<ComparablePoint> getSortedPointSet (Circle circle, Point referencePoint,
+            Collection<Point> intersectionPoints) {
+
+        SortedSet<ComparablePoint> sortedSet = new TreeSet<ComparablePoint>();
+
+        for (Point point : intersectionPoints) {
+            try {
+                double key = Geometrics.calculateRelativeAngle(circle.getCenter(),
+                        referencePoint, point);
+                ComparablePoint orderedPoint = new ComparablePoint(point,
+                        new DoubleKey(key));
+                sortedSet.add(orderedPoint);
+            }
+            catch (NullArgumentException e) {
+                // Should not catch this exception
+                e.printStackTrace();
+            }
+
+        }
+
+        return sortedSet;
+    }
+
+}
