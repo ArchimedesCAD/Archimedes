8f6d138accd5a6af6feb517591bf621da8b0eeb0
hugo.corbucci <hugo.corbucci@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-03-10 22:39:38 +0000

    Correcting a bug that stopped Trim to work on everything just because SemilineTrimmer was not a Trimmer. Hugo

--- br.org.archimedes.trims/trunk/src/br/org/archimedes/trim/SemilineTrimmer.java
+++ br.org.archimedes.trims/trunk/src/br/org/archimedes/trim/SemilineTrimmer.java
@@ -1,5 +1,23 @@
 package br.org.archimedes.trim;
 
-public class SemilineTrimmer {
+import java.util.Collection;
+
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Point;
+import br.org.archimedes.trims.interfaces.Trimmer;
+
+public class SemilineTrimmer implements Trimmer {
+
+    /* (non-Javadoc)
+     * @see br.org.archimedes.trims.interfaces.Trimmer#trim(br.org.archimedes.model.Element, java.util.Collection, br.org.archimedes.model.Point)
+     */
+    public Collection<Element> trim (Element element,
+            Collection<Element> references, Point click)
+            throws NullArgumentException {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
