1965aa7ec7a00e9d9762de9764154ddf2cb1c2b1
hugo.corbucci <hugo.corbucci@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-03-10 20:22:39 +0000

    Correcting the factory example plugin ID. (Caused the action not to work). Hugo (and Luiz)

--- br.org.archimedes.factoryexample/trunk/src/br/org/archimedes/factoryexample/Activator.java
+++ br.org.archimedes.factoryexample/trunk/src/br/org/archimedes/factoryexample/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "br.org.archimedes.wslOriginal";
+	public static final String PLUGIN_ID = "br.org.archimedes.factoryexample";
 
 	// The shared instance
 	private static Activator plugin;
