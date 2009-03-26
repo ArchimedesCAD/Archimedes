20bc708c415e6462d3e44f723466803fd7213158
rsider <rsider@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-03-26 17:01:52 +0000

    Trim de linha infinita corrigido. Falta: - corrigir testes; - refatorar.
    Kenzo e Wesley

--- br.org.archimedes.trims/trunk/src/br/org/archimedes/trim/InfiniteLineTrimmer.java
+++ br.org.archimedes.trims/trunk/src/br/org/archimedes/trim/InfiniteLineTrimmer.java
@@ -1,4 +1,3 @@
-
 package br.org.archimedes.trim;
 
 import java.util.ArrayList;
@@ -20,105 +19,130 @@ import br.org.archimedes.trims.interfaces.Trimmer;
 
 public class InfiniteLineTrimmer implements Trimmer {
 
-    private IntersectionManager intersectionManager;
+	private IntersectionManager intersectionManager;
+
+	public InfiniteLineTrimmer() {
+
+		intersectionManager = new IntersectionManagerEPLoader()
+				.getIntersectionManager();
+	}
+
+	public Collection<Element> trim(Element element,
+			Collection<Element> references, Point click)
+			throws NullArgumentException {
+
+		if (element == null || references == null) {
+			throw new NullArgumentException();
+		}
+
+		InfiniteLine xline = (InfiniteLine) element;
+		Collection<Element> trimResult = new ArrayList<Element>();
+		SortedSet<ComparablePoint> sortedPointSet = getSortedPointSet(xline,
+				xline.getInitialPoint(), intersectionManager
+						.getIntersectionsBetween(xline, references));
+
+		Vector direction = new Vector(xline.getInitialPoint(), xline
+				.getEndingPoint());
+		Vector clickVector = new Vector(xline.getInitialPoint(), click);
+		double key = direction.dotProduct(clickVector);
+		ComparablePoint clickPoint = null;
+		try {
+			clickPoint = new ComparablePoint(click, new DoubleKey(key));
+		} catch (NullArgumentException e) {
+			// Should never reach
+			e.printStackTrace();
+		}
+
+		SortedSet<ComparablePoint> headSet = sortedPointSet.headSet(clickPoint);
+		SortedSet<ComparablePoint> tailSet = sortedPointSet.tailSet(clickPoint);
+
+		try {
+			if (headSet.size() == 0 && tailSet.size() > 0) {
+				Point initialPoint = tailSet.first().getPoint();
+				
+				Vector director = new Vector(clickPoint.getPoint(),
+						initialPoint);
+				Point endPoint = new Point(initialPoint.getX()
+						+ director.getX(), initialPoint.getY()
+						+ director.getY());
+				
+				Element trimmedLine = new Semiline(initialPoint, endPoint);
+				
+				trimmedLine.setLayer(xline.getLayer());
+				trimResult.add(trimmedLine);
+
+			} else if (tailSet.size() == 0 && headSet.size() > 0) {
+				Point initialPoint = headSet.last().getPoint();
+				
+				Vector director = new Vector(clickPoint.getPoint(),
+						initialPoint);
+				Point endPoint = new Point(initialPoint.getX()
+						+ director.getX(), initialPoint.getY()
+						+ director.getY());
+
+				Element trimmedLine = new Semiline(initialPoint, endPoint);
+				
+				trimmedLine.setLayer(xline.getLayer());
+				trimResult.add(trimmedLine);
+
+			} else if (headSet.size() > 0 && tailSet.size() > 0) {
+				Point initialPoint1 = headSet.last().getPoint();
+				Vector director1 = new Vector(clickPoint.getPoint(),
+						initialPoint1);
+				Point endPoint1 = new Point(initialPoint1.getX()
+						+ director1.getX(), initialPoint1.getY()
+						+ director1.getY());
+
+				Element trimmedSemiLine1 = new Semiline(initialPoint1, endPoint1);
+				
+				trimmedSemiLine1.setLayer(xline.getLayer());
+				trimResult.add(trimmedSemiLine1);
 
 
-    public InfiniteLineTrimmer () {
+				Point initialPoint2 = tailSet.first().getPoint();
+				Vector director2 = new Vector(clickPoint.getPoint(),
+						initialPoint2);
+				Point endPoint2 = new Point(initialPoint2.getX()
+						+ director2.getX(), initialPoint2.getY()
+						+ director2.getY());
 
-        intersectionManager = new IntersectionManagerEPLoader()
-                .getIntersectionManager();
-    }
+				Element trimmedSemiLine2 = new Semiline(initialPoint2, endPoint2);
+				
+				trimmedSemiLine2.setLayer(xline.getLayer());
+				trimResult.add(trimmedSemiLine2);
+			}
+		} catch (Exception e) {
+			// Should not catch any exception
+			e.printStackTrace();
+		}
 
-    public Collection<Element> trim (Element element,
-            Collection<Element> references, Point click)
-            throws NullArgumentException {
+		return trimResult;
+	}
 
-        if (element == null || references == null) {
-            throw new NullArgumentException();
-        }
+	public SortedSet<ComparablePoint> getSortedPointSet(InfiniteLine line,
+			Point referencePoint, Collection<Point> intersectionPoints) {
 
-        InfiniteLine xline = (InfiniteLine) element;
-        Collection<Element> trimResult = new ArrayList<Element>();
-        SortedSet<ComparablePoint> sortedPointSet = getSortedPointSet(xline,
-                xline.getInitialPoint(), intersectionManager
-                        .getIntersectionsBetween(xline, references));
+		SortedSet<ComparablePoint> sortedPointSet = new TreeSet<ComparablePoint>();
 
-        Vector direction = new Vector(xline.getInitialPoint(), xline
-                .getEndingPoint());
-        Vector clickVector = new Vector(xline.getInitialPoint(), click);
-        double key = direction.dotProduct(clickVector);
-        ComparablePoint clickPoint = null;
-        try {
-            clickPoint = new ComparablePoint(click, new DoubleKey(key));
-        }
-        catch (NullArgumentException e) {
-            // Should never reach
-            e.printStackTrace();
-        }
+		Point otherPoint = line.getInitialPoint();
+		if (referencePoint.equals(line.getInitialPoint())) {
+			otherPoint = line.getEndingPoint();
+		}
 
-        SortedSet<ComparablePoint> headSet = sortedPointSet.headSet(clickPoint);
-        SortedSet<ComparablePoint> tailSet = sortedPointSet.tailSet(clickPoint);
+		Vector direction = new Vector(referencePoint, otherPoint);
+		for (Point point : intersectionPoints) {
+			Vector pointVector = new Vector(referencePoint, point);
+			double key = direction.dotProduct(pointVector);
+			ComparablePoint element;
+			try {
+				element = new ComparablePoint(point, new DoubleKey(key));
+				sortedPointSet.add(element);
+			} catch (NullArgumentException e) {
+				// Should never reach
+				e.printStackTrace();
+			}
+		}
 
-        try {
-            if (headSet.size() == 0 && tailSet.size() > 0) {
-                Element trimmedLine = new Semiline(tailSet.first().getPoint(),
-                        xline.getEndingPoint());
-                trimmedLine.setLayer(xline.getLayer());
-
-                trimResult.add(trimmedLine);
-            }
-            else if (tailSet.size() == 0 && headSet.size() > 0) {
-                Element trimmedLine = new Semiline(xline.getInitialPoint(),
-                        headSet.last().getPoint());
-                trimmedLine.setLayer(xline.getLayer());
-
-                trimResult.add(trimmedLine);
-            }
-            else if (headSet.size() > 0 && tailSet.size() > 0) {
-                Element trimmedSemiLine1 = new Semiline(
-                        headSet.last().getPoint(), xline.getInitialPoint());
-                trimmedSemiLine1.setLayer(xline.getLayer());
-                trimResult.add(trimmedSemiLine1);
-
-                Element trimmedSemiLine2 = new Semiline(tailSet.first()
-                        .getPoint(), xline.getEndingPoint());
-                trimmedSemiLine2.setLayer(xline.getLayer());
-                trimResult.add(trimmedSemiLine2);
-            }
-        }
-        catch (Exception e) {
-            // Should not catch any exception
-            e.printStackTrace();
-        }
-
-        return trimResult;
-    }
-
-    public SortedSet<ComparablePoint> getSortedPointSet (InfiniteLine line,
-            Point referencePoint, Collection<Point> intersectionPoints) {
-
-        SortedSet<ComparablePoint> sortedPointSet = new TreeSet<ComparablePoint>();
-
-        Point otherPoint = line.getInitialPoint();
-        if (referencePoint.equals(line.getInitialPoint())) {
-            otherPoint = line.getEndingPoint();
-        }
-
-        Vector direction = new Vector(referencePoint, otherPoint);
-        for (Point point : intersectionPoints) {
-            Vector pointVector = new Vector(referencePoint, point);
-            double key = direction.dotProduct(pointVector);
-            ComparablePoint element;
-            try {
-                element = new ComparablePoint(point, new DoubleKey(key));
-                sortedPointSet.add(element);
-            }
-            catch (NullArgumentException e) {
-                // Should never reach
-                e.printStackTrace();
-            }
-        }
-
-        return sortedPointSet;
-    }
+		return sortedPointSet;
+	}
 }
