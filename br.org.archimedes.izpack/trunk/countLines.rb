a3ad6680279751f3bc646c3a32941ce36ee02357
hugo.corbucci <hugo.corbucci@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-03-26 02:08:00 +0000

    Counting lines with the lib. Hugo

--- br.org.archimedes.izpack/trunk/countLines.rb
+++ br.org.archimedes.izpack/trunk/countLines.rb
@@ -1,67 +1,42 @@
 #!/usr/bin/ruby
 
-require 'find'
+require 'rubygems'
+require 'header-inserter/project'
+
+def count_line(filepath)
+    file = File.new filepath, "r"
+    file.inject(0){ |sum, line| sum + (/^\s*$/.match(line) ? 0 : 1) }
+end
+
+def print_stats(projects, file_type, data_name)
+    line_count = 0
+    file_count = 0
+    line_max = 0
+    line_min = 99999999999
+    projects.each do |project|
+        files = project.list(file_type).reject{|file| (not File.exist?(file.absolute_path)) or File.directory?(file.absolute_path) }
+        file_count += files.size
+        line_count += files.inject(0){|lines, file| lines + count_line(file.absolute_path) }
+        line_max = files.inject(line_max){|old_max, file|  max = old_max; count = count_line(file.absolute_path); max = count if count > max; max }
+        line_min = files.inject(line_min){|old_min, file|  min = old_min; count = count_line(file.absolute_path); min = count if count < min; min }
+    end
+    puts "Counted #{file_count} #{data_name} resulting in #{line_count} lines (mean is #{line_count/file_count} lines per files - min size is #{line_min} and max is #{line_max})."
+end
 
 dir = ARGV.first
 dir ||= "."
 
-def find_subdirs_matching(starting_dir, regexp)
-  dirs = []
-  Find.find(starting_dir) do |path|
-    if regexp.match path
-      dirs << path
-    end
-    if path != "."
-      Find.prune
-    end
-  end
-  return dirs
-end
+main_project = Project.new dir
+subprojects_files = main_project.list(/^br.org.archimedes[^\/]*$/)
 
-def find_files_within_matching(starting_dir, regexp)
-  files = []
-  Find.find(starting_dir) do |path|
-    if (not (File.directory? path)) and (regexp.match path)
-      files << path
-    end
-  end
-  return files
-end
+subprojects = subprojects_files.map{ |project_file| Project.new(project_file.absolute_path) }
+tests = subprojects.select{|p| /.*test(?:s)?$/.match p.path}
+code = subprojects - tests
 
-def count_lines_not_matching(file, regexp)
-  lines = []
-  File.new(file, "r").each do |line|
-    lines << line if not (regexp.match line)
-  end
-  return lines
-end
-
-def collect_stats(projects, file_regexp, files_type, line_regexp, lines_type)
-  file_count = 0
-  line_count = 0
-  projects.each do |project|
-    files = find_files_within_matching(project, file_regexp)
-#    puts "\t#{files.size} #{files_type} within #{project}" if files.size > 0
-
-    files.each do |file|
-      lines = count_lines_not_matching(file, line_regexp)
-#      puts "\t\t#{lines.size} #{lines_type} for #{file}" if lines.size > 0
-      line_count += lines.size
-    end
-    file_count += files.size
-  end
-  puts "#{file_count} #{files_type} for archimedes." if file_count > 0
-  puts "#{line_count} #{lines_type} for archimedes." if line_count > 0
-end
-
-projects = find_subdirs_matching(dir, /^#{dir}\/br\.org\.archimedes.*/)
-tests = projects.select{|p| /.*tests$/.match p}
-code = projects - tests
-
-puts "#{projects.size} projects created for archimedes: #{code.size} for code and #{tests.size} for tests."
+puts "#{subprojects.size} subprojects created for archimedes: #{code.size} for code and #{tests.size} for tests."
 puts ""
-collect_stats(code, /.*\.java$/, "java files", /^\w*$/, "LoC")
+print_stats(code, :java, "java files")
 puts ""
-collect_stats(tests, /.*\.java$/, "java test files", /^\w*$/, "test LoC")
+print_stats(tests, :java, "java test files")
 puts ""
-collect_stats(projects, /.*\.xml$/, "XML files", /^\w*$/, "XML LoC")
+print_stats(subprojects, :xml, "XML files")
