3171828c2326d622eff36d8ae15a7362b03563f0
rsider <rsider@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-03-31 18:52:44 +0000

    done klava, sider

--- br.org.archimedes.io.svg/trunk/src/br/org/archimedes/io/svg/elements/LeaderExporter.java
+++ br.org.archimedes.io.svg/trunk/src/br/org/archimedes/io/svg/elements/LeaderExporter.java
@@ -1,38 +1,45 @@
-/*
- * Created on Jun 23, 2008 for br.org.archimedes.io.pdf
+/**
+ * Copyright (c) 2009 Hugo Corbucci and others.<br>
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html<br>
+ * <br>
+ * Contributors:<br>
+ * Bruno Klava, Ricardo Sider - initial API and implementation<br>
+ * <br>
+ * This file was created on 2009/03/26, 12:05:56, by Ricardo Sider.<br>
+ * It is part of package br.org.archimedes.io.svg on the br.org.archimedes.io.svg project.<br>
  */
 
 package br.org.archimedes.io.svg.elements;
 
+import java.io.IOException;
+
+import br.org.archimedes.exceptions.NotSupportedException;
+import br.org.archimedes.interfaces.ElementExporter;
+import br.org.archimedes.leader.Leader;
+import br.org.archimedes.model.Rectangle;
 
 /**
- * Belongs to package br.org.archimedes.io.pdf.
+ * Belongs to package br.org.archimedes.io.svg.
  * 
- * @author night
+ * @author klava, sider
  */
-public class LeaderExporter /* implements ElementExporter<Leader> */ {
-    // TODO uncomment once leader exists.
+public class LeaderExporter implements ElementExporter<Leader> {
 
-    /* (non-Javadoc)
-     * @see br.org.archimedes.interfaces.ElementExporter#exportElement(br.org.archimedes.model.Element, java.lang.Object)
-     */
-    /*
-    public void exportElement (Leader leader, Object outputObject)
-            throws IOException {
-        PDFWriterHelper helper = (PDFWriterHelper) outputObject;
-        PdfContentByte cb = helper.getPdfContentByte();
+    public void exportElement (Leader element, Object outputObject) throws IOException,
+            NotSupportedException {
 
-        Point tip = leader.getTip();
-        Polyline pl = leader.getPolyLine();
-        PolylineExporter exporter = new PolylineExporter();
-        exporter.exportElement(pl, outputObject);
+        LineExporter lineExporter = new LineExporter();
+        lineExporter.exportElement(element.getPointer(), outputObject);
+        lineExporter.exportElement(element.getTextBase(), outputObject);
 
-        Point center = helper.modelToDocument(tip);
-        float centerX = (float) center.getX();
-        float centerY = (float) center.getY();
-        float radius = (float) Constant.LEADER_RADIUS;
-        cb.circle(centerX, centerY, radius);
+    }
 
-        cb.closePathFillStroke();
-    }*/
+    public void exportElement (Leader element, Object outputObject, Rectangle boundingBox)
+            throws IOException, NotSupportedException {
+
+        throw new NotSupportedException();
+
+    }
 }
