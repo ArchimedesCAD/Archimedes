4ae7a2e28fc62e21267b2da21969117a85befed0
dahora <dahora@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-03-31 17:22:01 +0000

    Testes para o Extend Manager concluidos Bruno da Hora e Luiz

---
+++ br.org.archimedes.extend.tests/trunk/plugin-test/br/org/archimedes/extend/ExtendManagerTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2009 Hugo Corbucci and others.<br>
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html<br>
+ * <br>
+ * Contributors:<br>
+ * Bruno V. da Hora - initial API and implementation<br>
+ * <br>
+ * This file was created on 2009/03/24, 16:01:03, by Bruno V. da Hora.<br>
+ * It is part of package br.org.archimedes.extend on the br.org.archimedes.extend.tests project.<br>
+ */
+package br.org.archimedes.extend;
+
+import br.org.archimedes.Tester;
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.extend.interfaces.Extender;
+import br.org.archimedes.line.Line;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Point;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class ExtendManagerTest extends Tester{
+    @Test
+    public void testeNaoModificaElementoComUmExtenderNaoExistente() throws Exception {
+        
+        MockExtenderEPLoader extenderEPLoader = new MockExtenderEPLoader(Collections.EMPTY_MAP);
+        ExtendManager manager = new ExtendManager(extenderEPLoader);
+        Line line = new Line(1.0, 0.0, -1.0, 0.0);
+        
+        manager.extend(line, Collections.EMPTY_LIST, new Point(0.0, 0.0));
+        assertEquals(line, new Line(1.0, 0.0, -1.0, 0.0));
+        
+    }
+    
+    @Test(expected = NullArgumentException.class)
+    public void testeChamaExtenderMockadoParaExtenderExistente() throws Exception {
+        
+        final Line line = new Line(1.0, 0.0, -1.0, 0.0);
+        final Collection<Element> reference = Collections.EMPTY_LIST;
+        final Point point = new Point(0.0, 0.0);
+        
+        Extender extenderMockado = new Extender() {         
+            
+            public void extend(Element element,
+                    Collection<Element> references, Point click)
+                    throws NullArgumentException {
+                    assertEquals(line, element);
+                    assertEquals(reference, references);
+                    assertEquals(point, click);
+                    throw new NullArgumentException();
+            }
+            
+        };
+        
+        HashMap<Class<? extends Element>, Extender> extenderMap = new HashMap<Class<? extends Element>, Extender>();
+        extenderMap.put(Line.class, extenderMockado);
+        
+        MockExtenderEPLoader extenderEPLoader = new MockExtenderEPLoader(extenderMap);
+        ExtendManager manager = new ExtendManager(extenderEPLoader);
+        
+        manager.extend(line, reference, point);
+    }
+}
---
+++ br.org.archimedes.extend.tests/trunk/plugin-test/br/org/archimedes/extend/MockExtenderEPLoader.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2009 Hugo Corbucci and others.<br>
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html<br>
+ * <br>
+ * Contributors:<br>
+ * Bruno V. da Hora - initial API and implementation<br>
+ * <br>
+ * This file was created on 2009/03/24, 16:01:03, by Bruno V. da Hora.<br>
+ * It is part of package br.org.archimedes.extend on the br.org.archimedes.extend.tests project.<br>
+ */
+package br.org.archimedes.extend;
+
+import br.org.archimedes.extend.interfaces.Extender;
+import br.org.archimedes.extend.rcp.ExtenderEPLoader;
+import br.org.archimedes.model.Element;
+
+import java.util.Map;
+
+public class MockExtenderEPLoader extends ExtenderEPLoader {
+    
+    private final Map<Class<? extends Element>, Extender> extenderOptions;
+
+    public MockExtenderEPLoader(Map<Class<? extends Element>, Extender> extenderOptions) {
+        this.extenderOptions = extenderOptions;
+    }
+
+    @Override
+    public Extender get(Class<? extends Element> elementClass) {
+         return extenderOptions.get(elementClass);
+    }
+
+}
