09a5132c05dddae97575ed4498d8ecab9c8a742f
hugo.corbucci <hugo.corbucci@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-04-03 12:57:40 +0000

    Adding a TODO to write tests for the PDF Exporting system. Hugo

---
+++ br.org.archimedes.io.pdf.tests/trunk/test/TODO.java
@@ -0,0 +1,22 @@
+
+/**
+ * Copyright (c) 2006, 2009 Hugo Corbucci and others.<br>
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html<br>
+ *<br>
+ * Contributors:<br>
+ * "Hugo Corbucci" - initial API and implementation<br>
+ * <br>
+ * This file was created on Apr 3, 2009, 9:56:44 AM.<br>
+ * It is part of  on the br.org.archimedes.io.pdf.tests project.<br>
+ */
+
+/**
+ * Belongs to package .
+ *
+ * @author "Hugo Corbucci"
+ */
+public class TODO {
+    // TODO Tests for the the PDF Exporter
+}
