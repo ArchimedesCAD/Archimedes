71636dc6fdf35bc2404b4ba49c8ac0058d90a695
hugo.corbucci <hugo.corbucci@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-04-03 17:22:16 +0000

    Adding a hashcode for line Hugo

--- br.org.archimedes.line/trunk/src/br/org/archimedes/line/Line.java
+++ br.org.archimedes.line/trunk/src/br/org/archimedes/line/Line.java
@@ -210,6 +210,19 @@ public class Line extends Element implements Offsetable {
         return (determinant >= 0);
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode () {
+
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.endingPoint == null) ? 0 : this.endingPoint.hashCode());
+        result = result + ((this.initialPoint == null) ? 0 : this.initialPoint.hashCode());
+        return result;
+    }
+
     /*
      * (non-Javadoc)
      * @see com.tarantulus.archimedes.model.Element#equals(com.tarantulus.archimedes.model.Element)
