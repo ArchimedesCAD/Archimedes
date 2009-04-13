be888a6a65709b72f9939a7de879a7c0d3bb4b55
hugo.corbucci <hugo.corbucci@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-04-13 12:03:27 +0000

    Adding support to Leader exporting in pdf. Hugo

--- br.org.archimedes.io.pdf/trunk/src/br/org/archimedes/io/pdf/elements/LeaderExporter.java
+++ br.org.archimedes.io.pdf/trunk/src/br/org/archimedes/io/pdf/elements/LeaderExporter.java
@@ -10,30 +10,41 @@
  * This file was created on 2008/06/23, 10:01:23, by Hugo Corbucci.<br>
  * It is part of package br.org.archimedes.io.pdf.elements on the br.org.archimedes.io.pdf project.<br>
  */
+
 package br.org.archimedes.io.pdf.elements;
 
+import br.org.archimedes.Constant;
+import br.org.archimedes.exceptions.NotSupportedException;
+import br.org.archimedes.interfaces.ElementExporter;
+import br.org.archimedes.io.pdf.PDFWriterHelper;
+import br.org.archimedes.leader.Leader;
+import br.org.archimedes.model.Point;
+import br.org.archimedes.model.Rectangle;
+
+import com.lowagie.text.pdf.PdfContentByte;
+
+import java.io.IOException;
 
 /**
  * Belongs to package br.org.archimedes.io.pdf.
  * 
- * @author night
+ * @author Hugo Corbucci
  */
-public class LeaderExporter /* implements ElementExporter<Leader> */ {
-    // TODO uncomment once leader exists.
+public class LeaderExporter implements ElementExporter<Leader> {
 
     /* (non-Javadoc)
      * @see br.org.archimedes.interfaces.ElementExporter#exportElement(br.org.archimedes.model.Element, java.lang.Object)
      */
-    /*
-    public void exportElement (Leader leader, Object outputObject)
-            throws IOException {
+    public void exportElement (Leader leader, Object outputObject) throws IOException,
+            NotSupportedException {
+
         PDFWriterHelper helper = (PDFWriterHelper) outputObject;
         PdfContentByte cb = helper.getPdfContentByte();
 
-        Point tip = leader.getTip();
-        Polyline pl = leader.getPolyLine();
-        PolylineExporter exporter = new PolylineExporter();
-        exporter.exportElement(pl, outputObject);
+        Point tip = leader.getPointer().getInitialPoint();
+        LineExporter exporter = new LineExporter();
+        exporter.exportElement(leader.getPointer(), outputObject);
+        exporter.exportElement(leader.getTextBase(), outputObject);
 
         Point center = helper.modelToDocument(tip);
         float centerX = (float) center.getX();
@@ -42,5 +53,14 @@ public class LeaderExporter /* implements ElementExporter<Leader> */ {
         cb.circle(centerX, centerY, radius);
 
         cb.closePathFillStroke();
-    }*/
+    }
+
+    /* (non-Javadoc)
+     * @see br.org.archimedes.interfaces.ElementExporter#exportElement(br.org.archimedes.model.Element, java.lang.Object, br.org.archimedes.model.Rectangle)
+     */
+    public void exportElement (Leader element, Object outputObject, Rectangle boundingBox)
+            throws IOException, NotSupportedException {
+
+        throw new NotSupportedException();
+    }
 }
