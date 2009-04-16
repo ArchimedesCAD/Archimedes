816f6f856722209cbbb3c65dc4d5bf4efd87a948
rsider <rsider@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-04-16 17:03:20 +0000

    Started implementing Semiline trimmer. Tests already written and should be working. Trim code squeleton ready but needs to check directions (head and tail). Ricardo and Kenzo

---
+++ br.org.archimedes.trims.tests/trunk/plugin-test/br/org/archimedes/trim/SemilineTrimTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2009 Hugo Corbucci and others.<br>
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html<br>
+ * <br>
+ * Contributors:<br>
+ * Hugo Corbucci - initial API and implementation<br>
+ * Kenzo Yamada, Ricardo Sider - later contributions<br>
+ * <br>
+ * This file was created on 2009/01/10, 11:16:48, by Hugo Corbucci.<br>
+ * It is part of package br.org.archimedes.trim on the br.org.archimedes.trims.tests project.<br>
+ */
+
+package br.org.archimedes.trim;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.org.archimedes.Tester;
+import br.org.archimedes.exceptions.InvalidArgumentException;
+import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.infiniteline.InfiniteLine;
+import br.org.archimedes.line.Line;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Point;
+import br.org.archimedes.semiline.Semiline;
+import br.org.archimedes.trims.interfaces.Trimmer;
+
+public class SemilineTrimTest extends Tester {
+
+    Trimmer trimmer = new SemilineTrimmer();
+
+    Collection<Element> references = new ArrayList<Element>();
+
+
+    @Override
+    public void setUp () throws NullArgumentException, InvalidArgumentException {
+
+        InfiniteLine y = new InfiniteLine(new Point(0.0, 0.0), new Point(0.0, 100.0));
+        InfiniteLine y2 = new InfiniteLine(new Point(50.0, 0.0), new Point(50.0, 10.0));
+        references.add(y);
+        references.add(y2);
+    }
+
+    @Test(expected = NullArgumentException.class)
+    public void testNullLineArgument () throws NullArgumentException {
+
+        trimmer.trim(null, references, new Point(0.0, 0.0));
+    }
+
+    @Test(expected = NullArgumentException.class)
+    public void testNullReferencesArgument () throws NullArgumentException,
+            InvalidArgumentException {
+
+        Semiline xline3 = new Semiline(new Point( -1.0, 2.0), new Point(3.0, 2.0));
+
+        trimmer.trim(xline3, null, new Point(0.0, 0.0));
+    }
+
+    @Test
+    public void semilineTrimsCenter () throws NullArgumentException, InvalidArgumentException {
+
+        Semiline horizontalLine = new Semiline(new Point( -1.0, 2.0), new Point(3.0, 2.0));
+        Collection<Element> collection = trimmer.trim(horizontalLine, references, new Point(1.0,
+                2.0));
+
+        assertCollectionContains(collection, new Line(new Point( -1.0, 2.0), new Point(0.0, 2.0)));
+
+        assertCollectionContains(collection, new Semiline(new Point(50.0, 2.0), new Point(100.0,
+                2.0)));
+
+        Assert.assertEquals(
+                "A trim between references should produce exactly 1 Line and 1 Semiline.", 2,
+                collection.size());
+    }
+
+    @Test
+    public void semilineTrimsFinitePart () throws NullArgumentException, InvalidArgumentException {
+
+        Semiline horizontalLine = new Semiline(new Point( -1.0, 2.0), new Point(3.0, 2.0));
+        Collection<Element> collection = trimmer.trim(horizontalLine, references, new Point( -0.5,
+                2.0));
+
+        assertCollectionContains(collection,
+                new Semiline(new Point(0.0, 2.0), new Point(10.0, 2.0)));
+
+        Assert.assertEquals("A trim between references should produce exactly 1 Semiline.", 1,
+                collection.size());
+    }
+
+    @Test
+    public void semilineTrimsInfinitePart () throws NullArgumentException, InvalidArgumentException {
+
+        Semiline horizontalLine = new Semiline(new Point( -1.0, 2.0), new Point(3.0, 2.0));
+        Collection<Element> collection = trimmer.trim(horizontalLine, references, new Point(69.0,
+                2.0));
+
+        assertCollectionContains(collection, new Line(new Point( -1.0, 2.0), new Point(50.0, 2.0)));
+
+        Assert.assertEquals("A trim between references should produce exactly 1 Line.", 1,
+                collection.size());
+    }
+
+}
--- br.org.archimedes.trims/trunk/src/br/org/archimedes/trim/SemilineTrimmer.java
+++ br.org.archimedes.trims/trunk/src/br/org/archimedes/trim/SemilineTrimmer.java
@@ -5,31 +5,138 @@
  * http://www.eclipse.org/legal/epl-v10.html<br>
  * <br>
  * Contributors:<br>
- * Hugo Corbucci - initial API and implementation<br>
+ * Ricardo Sider, Kenzo Yamada - initial API and implementation<br>
  * <br>
  * This file was created on 2009/01/10, 11:16:48, by Hugo Corbucci.<br>
  * It is part of package br.org.archimedes.trim on the br.org.archimedes.trims project.<br>
  */
+
 package br.org.archimedes.trim;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.interfaces.IntersectionManager;
+import br.org.archimedes.line.Line;
+import br.org.archimedes.model.ComparablePoint;
+import br.org.archimedes.model.DoubleKey;
 import br.org.archimedes.model.Element;
 import br.org.archimedes.model.Point;
+import br.org.archimedes.model.Vector;
+import br.org.archimedes.rcp.extensionpoints.IntersectionManagerEPLoader;
+import br.org.archimedes.semiline.Semiline;
 import br.org.archimedes.trims.interfaces.Trimmer;
 
 public class SemilineTrimmer implements Trimmer {
 
-    /* (non-Javadoc)
-     * @see br.org.archimedes.trims.interfaces.Trimmer#trim(br.org.archimedes.model.Element, java.util.Collection, br.org.archimedes.model.Point)
+    private IntersectionManager intersectionManager;
+
+
+    public SemilineTrimmer () {
+
+        intersectionManager = new IntersectionManagerEPLoader().getIntersectionManager();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see br.org.archimedes.trims.interfaces.Trimmer#trim(br.org.archimedes.model.Element,
+     * java.util.Collection, br.org.archimedes.model.Point)
      */
-    public Collection<Element> trim (Element element,
-            Collection<Element> references, Point click)
+    public Collection<Element> trim (Element element, Collection<Element> references, Point click)
             throws NullArgumentException {
 
-        // TODO Auto-generated method stub
-        return null;
+        if (element == null || references == null) {
+            throw new NullArgumentException();
+        }
+
+        Semiline line = (Semiline) element;
+        Collection<Element> trimResult = new ArrayList<Element>();
+
+        SortedSet<ComparablePoint> sortedPointSet = getSortedPointSet(line, line.getInitialPoint(),
+                intersectionManager.getIntersectionsBetween(line, references));
+
+        Vector direction = new Vector(line.getInitialPoint(), line.getDirectionPoint());
+
+        Vector clickVector = new Vector(line.getInitialPoint(), click);
+        double key = direction.dotProduct(clickVector);
+        ComparablePoint clickPoint = null;
+        try {
+            clickPoint = new ComparablePoint(click, new DoubleKey(key));
+        }
+        catch (NullArgumentException e) {
+            // Should never reach
+            e.printStackTrace();
+        }
+
+        SortedSet<ComparablePoint> headSet = sortedPointSet.headSet(clickPoint);
+        SortedSet<ComparablePoint> tailSet = sortedPointSet.tailSet(clickPoint);
+
+        try {
+
+            if (headSet.size() == 0 && tailSet.size() > 0) {
+                Point initialPoint = tailSet.first().getPoint();
+                Element trimmedLine = new Line(line.getInitialPoint(), initialPoint);
+                trimmedLine.setLayer(line.getLayer());
+                trimResult.add(trimmedLine);
+
+            }
+            else if (tailSet.size() == 0 && headSet.size() > 0) {
+                Point initialPoint = headSet.last().getPoint();
+                Element trimmedLine = new Semiline(initialPoint, line.getDirectionPoint());
+                trimmedLine.setLayer(line.getLayer());
+                trimResult.add(trimmedLine);
+
+            }
+            else if (headSet.size() > 0 && tailSet.size() > 0) {
+
+                Point initialPoint = headSet.last().getPoint();
+                Element trimmedLine = new Semiline(initialPoint, line.getDirectionPoint());
+                trimmedLine.setLayer(line.getLayer());
+                trimResult.add(trimmedLine);
+
+                initialPoint = tailSet.first().getPoint();
+                trimmedLine = new Line(line.getInitialPoint(), initialPoint);
+                trimmedLine.setLayer(line.getLayer());
+                trimResult.add(trimmedLine);
+            }
+        }
+        catch (Exception e) {
+            // Should not catch any exception
+            e.printStackTrace();
+        }
+
+        return trimResult;
+    }
+
+    public SortedSet<ComparablePoint> getSortedPointSet (Semiline line, Point referencePoint,
+            Collection<Point> intersectionPoints) {
+
+        SortedSet<ComparablePoint> sortedPointSet = new TreeSet<ComparablePoint>();
+
+        Point otherPoint = line.getInitialPoint();
+        if (referencePoint.equals(line.getInitialPoint())) {
+            otherPoint = line.getDirectionPoint();
+        }
+
+        Vector direction = new Vector(referencePoint, otherPoint);
+        for (Point point : intersectionPoints) {
+            Vector pointVector = new Vector(referencePoint, point);
+            double key = direction.dotProduct(pointVector);
+            ComparablePoint element;
+            try {
+                element = new ComparablePoint(point, new DoubleKey(key));
+                sortedPointSet.add(element);
+            }
+            catch (NullArgumentException e) {
+                // Should never reach
+                e.printStackTrace();
+            }
+        }
+
+        return sortedPointSet;
     }
 
 }
