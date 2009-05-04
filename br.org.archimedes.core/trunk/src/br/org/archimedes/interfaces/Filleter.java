de9830d6a547f70bf270fcdd408c1a25c1d5dde4
lreal <lreal@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-05-04 23:03:48 +0000

    Extension point for the Fillet Luiz Real, Bruno Klava

---
+++ br.org.archimedes.core/trunk/src/br/org/archimedes/interfaces/Filleter.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2007, 2009 Hugo Corbucci and others.<br>
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html<br>
+ * <br>
+ * Contributors:<br>
+ * Luiz Real, Bruno Klava - initial API and implementation<br>
+ * <br>
+ * This file was created on 2009/05/04, 20:02:00, by Luiz Real.<br>
+ * It is part of package br.org.archimedes.interfaces on the br.org.archimedes.core project.<br>
+ */
+package br.org.archimedes.interfaces;
+
+/**
+ * Belongs to package br.org.archimedes.interfaces.
+ * 
+ * @author Luiz Real, Bruno Klava
+ */
+public interface Filleter {
+    // TODO shall the fillet return a collection of elements or nothing?
+}
