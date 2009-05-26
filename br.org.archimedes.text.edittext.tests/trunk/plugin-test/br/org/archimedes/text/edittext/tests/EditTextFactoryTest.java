fac3de3b31e327285abaedca1ca5ae4c8a15eb14
rsider <rsider@a010ea30-1657-0410-bcc9-f6899aecc918>
2009-05-26 19:02:04 +0000

    Added edittext tests to projectSet More tests for edittext
    seidel, sider

---
+++ br.org.archimedes.text.edittext.tests/trunk/plugin-test/br/org/archimedes/text/edittext/tests/EditTextFactoryTest.java
@@ -0,0 +1,75 @@
+
+package br.org.archimedes.text.edittext.tests;
+
+import br.org.archimedes.exceptions.InvalidArgumentException;
+import br.org.archimedes.gui.opengl.Color;
+import br.org.archimedes.helper.FactoryTester;
+import br.org.archimedes.model.Drawing;
+import br.org.archimedes.model.Element;
+import br.org.archimedes.model.Layer;
+import br.org.archimedes.model.LineStyle;
+import br.org.archimedes.model.Point;
+import br.org.archimedes.model.Selection;
+import br.org.archimedes.text.Text;
+import br.org.archimedes.text.edittext.EditTextFactory;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EditTextFactoryTest extends FactoryTester {
+
+    EditTextFactory factory;
+
+    Drawing drawing;
+
+    Layer layer;
+
+    Text text;
+
+
+    @Before
+    public void setUp () throws Exception {
+
+        layer = new Layer(new Color(0.0, 0.0, 0.0), "layer1", LineStyle.CONTINUOUS, 1.0);
+        layer.setLocked(false);
+        text = new Text("Olá Mundo", new Point(0, 0), 10.0);
+        layer.putElement(text);
+        drawing = new Drawing("Testes");
+        br.org.archimedes.Utils.getController().setActiveDrawing(drawing);
+    }
+
+    @After
+    public void tearDown () throws Exception {
+
+        br.org.archimedes.Utils.getController().setActiveDrawing(null);
+    }
+
+    @Test(expected = InvalidArgumentException.class)
+    public void throwsInvalidArgumentExceptionIfThereIsNoTextUnderTheDrawing ()
+            throws InvalidArgumentException {
+
+        drawing.addLayer(layer);
+        factory = new EditTextFactory(new Point(500, 500));
+    }
+    
+    @Test
+    public void testAcceptsSetOfElements() {
+        factory = new EditTextFactory();
+        assertBegin(factory, false);
+        
+        assertInvalidNext(factory, null);
+        assertInvalidNext(factory, new Point(0,0));
+        assertInvalidNext(factory, new Object());
+        assertInvalidNext(factory, new Selection());
+        
+        Set<Element> set = new HashSet<Element>();
+        assertInvalidNext(factory, set);
+        
+        set.add(text);
+        assertSafeNext(factory, set, true);
+    }
+}
