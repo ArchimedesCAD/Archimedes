f3f09204cf79b6a6963d177e70aa36d8b6320f27
Calebe de Paula Bianchini <calebe.paula.bianchini@gmail.com>
2012-05-16 15:28:01 -0300

    Implemented two tests for circle

--- br.org.archimedes.circle.tests/test/br/org/archimedes/circle/CircleTest.java
+++ br.org.archimedes.circle.tests/test/br/org/archimedes/circle/CircleTest.java
@@ -19,17 +19,23 @@ import br.org.archimedes.gui.opengl.Color;
 import br.org.archimedes.model.Layer;
 import br.org.archimedes.model.LineStyle;
 import br.org.archimedes.model.Point;
+import br.org.archimedes.model.Rectangle;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 public class CircleTest {
 	
 	private static final int CIRCLE_RADIUS = 1;
-	private Circle circle;
+	private Circle circle1;
+	private Circle circle2;
 	private Point externalPoint;
 	private Point internalPoint;
 	private Point centerPoint;
@@ -41,32 +47,56 @@ public class CircleTest {
 		internalPoint = new Point(0, 0.5);
 		centerPoint = new Point(0, 0);
 		
-		circle = new Circle(centerPoint, Double.valueOf(CIRCLE_RADIUS));
+		circle1 = new Circle(centerPoint, Double.valueOf(CIRCLE_RADIUS));
+		circle2 = new Circle(new Point(4, 3), 2.0);
 	}
 	
 	@Test
 	public void testProjectionOfAPointOnTheCircle() throws NullArgumentException {
 
-	    Assert.assertEquals(new Point(0, 1), circle.getProjectionOf(externalPoint));
+	    Assert.assertEquals(new Point(0, 1), circle1.getProjectionOf(externalPoint));
 		
-		Assert.assertEquals(new Point(0, 1), circle.getProjectionOf(internalPoint));
+		Assert.assertEquals(new Point(0, 1), circle1.getProjectionOf(internalPoint));
 
-		Assert.assertEquals(new Point(1, 0), circle.getProjectionOf(centerPoint));
+		Assert.assertEquals(new Point(1, 0), circle1.getProjectionOf(centerPoint));
 	}
 	
 	@Test
     public void cloningKeepsTheSameLayer () throws Exception {
 
         Layer layer = new Layer(new Color(0, 200, 0), "layer", LineStyle.CONTINUOUS, 1);
-        circle.setLayer(layer);
-        Circle clone = (Circle) circle.clone();
+        circle1.setLayer(layer);
+        Circle clone = (Circle) circle1.clone();
         
         assertEquals(layer, clone.getLayer());
     }
 	
-	// TODO Test equals and hash code of a circle
+	@Test
+	public void testEqualsCircle() throws Exception {
+		assertTrue(circle1.equals( new Circle( new Point(0,0) , Double.valueOf(CIRCLE_RADIUS) ) ));
+		assertTrue(circle2.equals( new Circle( new Point(4,3) , 2.0 ) ));
+	}
+	
+	@Test
+	public void testHashcodeCircle() throws Exception {
+		int hash1 = circle1.hashCode();
+		int hash2 = circle2.hashCode();
+		assertEquals(hash1, circle1.hashCode()); // Test if it returns the same code again
+		assertFalse(hash1 == hash2);
+		
+		Circle c1 = new Circle(centerPoint, Double.valueOf(CIRCLE_RADIUS));
+		int hash_c1 = c1.hashCode();
+		assertEquals(hash1, hash_c1);
+		c1.move(5, 0);
+		assertFalse(hash_c1 == c1.hashCode()); // Object changed, hash should change
+	}
 	
 	// TODO Test the boundary rectangle of a circle
+	@Test
+	public void testBoundaryRectangle() {
+		assertEquals(circle1.getBoundaryRectangle(), new Rectangle(-1, -1, 1, 1));
+		assertEquals(circle2.getBoundaryRectangle(), new Rectangle(2, 1, 6, 5));
+	}
 	
 	// TODO Test the reference points of a circle
 	
