fe76c3fe6ba8037c5a9e76b74ff4ba360c412193
Calebe de Paula Bianchini <calebe.paula.bianchini@gmail.com>
2012-05-25 17:23:43 -0300

    Added an 2x1 ellipse test for EllipseInfiniteLineIntersectorTest.

--- br.org.archimedes.intersections.tests/test/br/org/archimedes/intersectors/EllipseInfiniteLineIntersectorTest.java
+++ br.org.archimedes.intersections.tests/test/br/org/archimedes/intersectors/EllipseInfiniteLineIntersectorTest.java
@@ -17,7 +17,9 @@ import br.org.archimedes.model.Point;
 
 public class EllipseInfiniteLineIntersectorTest extends Tester {
 
-	private Ellipse ellipse1x1, ellipse2x2, ellipse2x1;
+	// TODO Create a test using an ellipse outside the origin
+	// TODO Create a test using a rotated ellipse
+	private Ellipse ellipse1x1, ellipse2x2, ellipse2x1;//, ellipse2x3;
 	private EllipseInfiniteLineIntersector intersector;
 
 	@Before
@@ -26,6 +28,9 @@ public class EllipseInfiniteLineIntersectorTest extends Tester {
 				1));
 		ellipse2x2 = new Ellipse(new Point(0, 0), new Point(2, 0), new Point(0,
 				2));
+		ellipse2x1 = new Ellipse(new Point(0, 0), new Point(2, 0), new Point(0,
+				1));
+
 		intersector = new EllipseInfiniteLineIntersector();
 	}
 
@@ -37,6 +42,8 @@ public class EllipseInfiniteLineIntersectorTest extends Tester {
 				intersector.getIntersections(ellipse1x1, verticalLine));
 		assertCollectionTheSame(new ArrayList<Point>(),
 				intersector.getIntersections(ellipse2x2, verticalLine));
+		assertCollectionTheSame(new ArrayList<Point>(),
+				intersector.getIntersections(ellipse2x1, verticalLine));
 	}
 
 	@Test
@@ -80,6 +87,26 @@ public class EllipseInfiniteLineIntersectorTest extends Tester {
 	}
 
 	@Test
+	public void shouldReturnOneTangencyPoint2x1()
+			throws InvalidArgumentException, NullArgumentException {
+		InfiniteLine verticalLine = new InfiniteLine(2.0, 0.0, 2.0, 1.0);
+
+		ArrayList<Point> interPoints = new ArrayList<Point>();
+		interPoints.add(new Point(2.0, 0.0));
+
+		assertCollectionTheSame(interPoints,
+				intersector.getIntersections(ellipse2x1, verticalLine));
+
+		InfiniteLine horizontalLine = new InfiniteLine(0.0, 1.0, 1.0, 1.0);
+
+		interPoints = new ArrayList<Point>();
+		interPoints.add(new Point(0.0, 1.0));
+
+		assertCollectionTheSame(interPoints,
+				intersector.getIntersections(ellipse2x1, horizontalLine));
+	}
+
+	@Test
 	public void shouldReturnOneDiagonalTangencyPoint1x1()
 			throws InvalidArgumentException, NullArgumentException {
 		double sqrt2 = Math.sqrt(2);
@@ -109,25 +136,52 @@ public class EllipseInfiniteLineIntersectorTest extends Tester {
 	}
 
 	@Test
+	public void shouldReturnOneDiagonalTangencyPoint2x1()
+			throws InvalidArgumentException, NullArgumentException {
+		
+		// FIXME: this test is probably wrong.
+		
+		double sqrt3 = Math.sqrt(3.0);
+		InfiniteLine diagonalLine = new InfiniteLine(10, 0.0, 1.0, sqrt3 / 2.0);
+
+		ArrayList<Point> interPoints = new ArrayList<Point>();
+		interPoints = new ArrayList<Point>();
+		interPoints.add(new Point(1, sqrt3 / 2.0));
+
+		assertCollectionTheSame(interPoints,
+				intersector.getIntersections(ellipse2x1, diagonalLine));
+	}
+
+	@Test
 	public void shouldReturnTwoTangecyPoint() throws InvalidArgumentException,
 			NullArgumentException {
 		InfiniteLine verticalLine = new InfiniteLine(0.0, 0.0, 0.0, 1.0);
-
-		ArrayList<Point> interPoints = new ArrayList<Point>();
-		interPoints.add(new Point(0.0, -1.0));
-		interPoints.add(new Point(0.0, 1.0));
-
-		assertCollectionTheSame(interPoints,
-				intersector.getIntersections(ellipse1x1, verticalLine));
-
 		InfiniteLine horizontalLine = new InfiniteLine(0.0, 0.0, 1.0, 0.0);
 
-		interPoints = new ArrayList<Point>();
-		interPoints.add(new Point(-1.0, 0.0));
-		interPoints.add(new Point(1.0, 0.0));
+		assertCollectionTheSame(generateList(0.0, -1.0, 0.0, 1.0),
+				intersector.getIntersections(ellipse1x1, verticalLine));
 
-		assertCollectionTheSame(interPoints,
+		assertCollectionTheSame(generateList(-1.0, 0.0, 1.0, 0.0),
 				intersector.getIntersections(ellipse1x1, horizontalLine));
+		
+		assertCollectionTheSame(generateList(0.0, -2.0, 0.0, 2.0),
+				intersector.getIntersections(ellipse2x2, verticalLine));
+		
+		assertCollectionTheSame(generateList(-2.0, 0.0, 2.0, 0.0),
+				intersector.getIntersections(ellipse2x2, horizontalLine));
+		
+		assertCollectionTheSame(generateList(0.0, -1.0, 0.0, 1.0),
+				intersector.getIntersections(ellipse2x1, verticalLine));
+		
+		assertCollectionTheSame(generateList(-2.0, 0.0, 2.0, 0.0),
+				intersector.getIntersections(ellipse2x1, horizontalLine));
+	}
+	
+	private ArrayList<Point> generateList(double x1, double y1, double x2, double y2) {
+		ArrayList<Point> interPoints = new ArrayList<Point>();
+		interPoints.add(new Point(x1, y1));
+		interPoints.add(new Point(x2, y2));
+		return interPoints;
 	}
 
 }
--- br.org.archimedes.intersections/src/br/org/archimedes/intersectors/EllipseLineIntersector.java
+++ br.org.archimedes.intersections/src/br/org/archimedes/intersectors/EllipseLineIntersector.java
@@ -35,70 +35,9 @@ public class EllipseLineIntersector implements Intersector {
 			line = (Line) otherElement;
 			ellipse = (Ellipse) element;
 		}
-				
-		// Ellipse description: (x - x0)²/a² + (y - y0)²/b² = 1
-		// Line: p + v * d, d > 0
-		// A point L is in the Line-Ellipse intersection iff it satisfies both equations
 		
-		double theta = ellipse.getFi();
-				
-		Point lineP = line.getInitialPoint();
-		Vector lineD = new Vector(lineP, line.getEndingPoint()).normalized();
+		//TODO: use EllipseInfiniteLineIntersector
 		
-		double ellipseA = ellipse.getSemiMajorAxis().getNorm();
-		double ellipseB = ellipse.getSemiMinorAxis().getNorm();
-		Point ellipseCenter = ellipse.getCenter();
-		
-		double ellipseAsqr = ellipseA * ellipseA;
-		double ellipseBsqr = ellipseB * ellipseB;
-		
-		double Ox = lineP.getX() - ellipseCenter.getX();
-		double Oy = lineP.getY() - ellipseCenter.getY();
-		
-		double sinTheta = Math.sin(theta);
-		double cosTheta = Math.cos(theta);
-		
-		double L1 = ellipseBsqr * cosTheta * cosTheta + ellipseAsqr * sinTheta * sinTheta; // L1 = b² (se Fi = 0)
-		double L2 = ellipseBsqr * sinTheta * sinTheta + ellipseAsqr * cosTheta * cosTheta; // L2 = a² (se Fi = 0)
-		
-		double Dx = lineD.getX();
-		double Dy = lineD.getY();
-		
-		double solutionA = (Dx*Dx * L1) + (Dy*Dy * L2) + (2 * Dx * Dy * (ellipseAsqr - ellipseBsqr) * sinTheta * cosTheta);
-		double solutionB = (2 * Ox * Dx * L1) + (2 * Oy * Dy * L2) + (2 * (Ox * Dy + Oy * Dx) * (ellipseAsqr - ellipseBsqr) * sinTheta * cosTheta);
-		double solutionC = (Ox*Ox * L1) + (Oy*Oy * L2) + (2 * Ox * Oy * (ellipseAsqr - ellipseBsqr) * sinTheta * cosTheta) - ellipseAsqr * ellipseBsqr;
-
-		try {
-			ArrayList<Double> solutions = (ArrayList<Double>) solve(solutionA, solutionB, solutionC);
-			for(Double sol : solutions) {
-				Point p = lineP.addVector(lineD.multiply(sol));
-				//if(line.contains(p))
-					intersections.add(p);
-			}
-			
-		} catch (InvalidArgumentException e) {
-            e.printStackTrace();
-        }
 		return intersections;
 	}
-	
-    private Collection<Double> solve(double a, double b, double c) throws InvalidArgumentException
-    {
-        if (Math.abs(a) < Constant.EPSILON)
-            throw new InvalidArgumentException();
-        double delta = delta(a, b, c);
-        ArrayList<Double> solutions = new ArrayList<Double>();
-        if (delta < -Constant.EPSILON)
-            return solutions;
-        solutions.add((-b + Math.sqrt(delta))/2*a);
-        if (delta > Constant.EPSILON)
-            solutions.add((-b - Math.sqrt(delta))/2*a);
-        return solutions;
-    }
-    
-    private double delta(double a, double b, double c)
-    {
-        return b*b - 4*a*c;
-    }
-
 }
