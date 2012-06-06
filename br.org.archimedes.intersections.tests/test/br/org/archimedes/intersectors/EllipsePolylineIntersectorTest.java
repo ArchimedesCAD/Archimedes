c5afbfa93e549b2eb706eaa439df9591d6350666
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-06-06 15:58:42 -0300

    Starting EllipsePolyLineIntersector

--- br.org.archimedes.intersections.tests/test/br/org/archimedes/intersectors/EllipsePolylineIntersectorTest.java
+++ br.org.archimedes.intersections.tests/test/br/org/archimedes/intersectors/EllipsePolylineIntersectorTest.java
@@ -1,24 +1,40 @@
 package br.org.archimedes.intersectors;
 
+import java.util.Collections;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import br.org.archimedes.Tester;
 import br.org.archimedes.ellipse.Ellipse;
 import br.org.archimedes.exceptions.InvalidArgumentException;
 import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.line.Line;
 import br.org.archimedes.model.Point;
+import br.org.archimedes.polyline.Polyline;
 
-public class EllipsePolylineIntersectorTest extends Tester{
-	
+public class EllipsePolylineIntersectorTest extends Tester {
+
 	Ellipse ellipse;
 	EllipsePolylineIntersector intersector;
-	
+
 	@Before
 	public void setUp() throws NullArgumentException, InvalidArgumentException {
-		ellipse = new Ellipse(new Point(0.0, 0.0), new Point(1.0, 0.0), new Point(0.0, 1.0));
+		ellipse = new Ellipse(new Point(0.0, 0.0), new Point(1.0, 0.0),
+				new Point(0.0, 1.0));
 		intersector = new EllipsePolylineIntersector();
 	}
-	
-	
-	
+
+	@Test
+	public void shouldNotReturnIntersectionPoints()
+			throws InvalidArgumentException, NullArgumentException {
+
+		Polyline polyline = new Polyline(new Point(2.0, 0.0), new Point(2.0,
+				2.0), new Point(0.0, 2.0));
+
+		assertCollectionTheSame(Collections.emptyList(),
+				intersector.getIntersections(ellipse, polyline));
+
+	}
+
 }
--- br.org.archimedes.intersections/src/br/org/archimedes/intersectors/EllipsePolylineIntersector.java
+++ br.org.archimedes.intersections/src/br/org/archimedes/intersectors/EllipsePolylineIntersector.java
@@ -1,18 +1,39 @@
 package br.org.archimedes.intersectors;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+import br.org.archimedes.ellipse.Ellipse;
+import br.org.archimedes.exceptions.InvalidArgumentException;
 import br.org.archimedes.exceptions.NullArgumentException;
+import br.org.archimedes.infiniteline.InfiniteLine;
 import br.org.archimedes.intersections.interfaces.Intersector;
+import br.org.archimedes.line.Line;
 import br.org.archimedes.model.Element;
 import br.org.archimedes.model.Point;
+import br.org.archimedes.polyline.Polyline;
 
 public class EllipsePolylineIntersector implements Intersector{
 
 	public Collection<Point> getIntersections(Element element,
 			Element otherElement) throws NullArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		if (element == null || otherElement == null)
+			throw new NullArgumentException();
+		
+		Polyline polyline;
+		Ellipse ellipse;
+
+		if (element.getClass() == Line.class) {
+			polyline = (Polyline) element;
+			ellipse = (Ellipse) otherElement;
+		} else {
+			polyline = (Polyline) otherElement;
+			ellipse = (Ellipse) element;
+		}
+		
+		Collection<Point> results = new ArrayList<Point>();
+		
+		return results;
 	}
 
 }
