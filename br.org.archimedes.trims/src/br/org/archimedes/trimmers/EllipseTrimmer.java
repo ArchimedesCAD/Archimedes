1a46e3064c0766e0db8277a6df0b19bf423d9d75
Calebe de Paula Bianchini <calebe.paula.bianchini@gmail.com>
2012-06-27 15:45:18 -0300

    Initial implementation for EllipseTrim using Arc.

--- br.org.archimedes.trims/src/br/org/archimedes/trimmers/EllipseTrimmer.java
+++ br.org.archimedes.trims/src/br/org/archimedes/trimmers/EllipseTrimmer.java
@@ -15,6 +15,7 @@ import br.org.archimedes.model.ComparablePoint;
 import br.org.archimedes.model.DoubleKey;
 import br.org.archimedes.model.Element;
 import br.org.archimedes.model.Point;
+import br.org.archimedes.model.Vector;
 import br.org.archimedes.trims.interfaces.Trimmer;
 
 public class EllipseTrimmer implements Trimmer {
@@ -32,19 +33,63 @@ public class EllipseTrimmer implements Trimmer {
 		}
 		Ellipse ellipse = (Ellipse) element;
 
+		System.out.println("cut: " + cutPoints);
+		System.out.println("click: " + click);
+
 		Collection<Element> trimResult = new ArrayList<Element>();
 
 		if (cutPoints.size() <= 1)
 			return Collections.singleton(element);
-		// TODO: Create a method to ordenate a list of points relatively to the ellipse.
-		SortedSet<ComparablePoint> sortedPointSet = getSortedPointSet(ellipse,
-				ellipse.getHeightPoint(), cutPoints);
+		// TODO: Create a method to ordenate a list of points relatively to the
+		// ellipse.
+		SortedSet<ComparablePoint> sortedPointSet = getSortedPointSet(
+				ellipse,
+				ellipse.getCenter().clone()
+						.addVector(ellipse.getSemiMajorAxis()), cutPoints);
+		ComparablePoint clickPoint = null;
+
+		Point initialPoint = ellipse.getCenter().clone();
+		initialPoint.addVector(ellipse.getSemiMajorAxis());
+
+		try {
+			double key = Geometrics.calculateRelativeAngle(ellipse.getCenter(),
+					initialPoint, click);
+			clickPoint = new ComparablePoint(click, new DoubleKey(key));
+		} catch (NullArgumentException e) {
+			// Should never reach
+			e.printStackTrace();
+		}
+
+		SortedSet<ComparablePoint> negativeIntersections = sortedPointSet
+				.headSet(clickPoint);
+		SortedSet<ComparablePoint> positiveIntersections = sortedPointSet
+				.tailSet(clickPoint);
+
+		Point first = null;
+		Point last = null;
+		if (positiveIntersections.size() > 0)
+			first = positiveIntersections.first().getPoint();
+		else
+			first = negativeIntersections.first().getPoint();
+		if (negativeIntersections.size() > 0)
+			last = negativeIntersections.last().getPoint();
+		else
+			last = positiveIntersections.last().getPoint();
+
+		try {
+			System.out.println("first = " + first + "; last = " + last);
+			Vector radius = new Vector(first, last);
+			Element arc = new Arc(first, last, first.clone().addVector(
+					radius.multiply(0.5)), true);
+			trimResult.add(arc);
+		} catch (InvalidArgumentException e) {
+			e.printStackTrace();
+		}
 
 		return trimResult;
-
 	}
 
-	private SortedSet<ComparablePoint> getSortedPointSet(Ellipse circle,
+	private SortedSet<ComparablePoint> getSortedPointSet(Ellipse ellipse,
 			Point referencePoint, Collection<Point> cutPoints) {
 
 		SortedSet<ComparablePoint> sortedSet = new TreeSet<ComparablePoint>();
@@ -52,7 +97,7 @@ public class EllipseTrimmer implements Trimmer {
 		for (Point point : cutPoints) {
 			try {
 				double key = Geometrics.calculateRelativeAngle(
-						circle.getCenter(), referencePoint, point);
+						ellipse.getCenter(), referencePoint, point);
 				ComparablePoint orderedPoint = new ComparablePoint(point,
 						new DoubleKey(key));
 				sortedSet.add(orderedPoint);
