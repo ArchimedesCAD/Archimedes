24b20f9a04e7fab8ceac15d7ab1f504efe72012e
RobertoRodrigues <me@rrodrigues.net>
2013-02-14 16:44:40 -0200

    finished implementation of tests and  method contains of ellipticArc

--- br.org.archimedes.ellipticarc.tests/src/br/org/archimedes/ellipticarc/tests/EllipticArcTest.java
+++ br.org.archimedes.ellipticarc.tests/src/br/org/archimedes/ellipticarc/tests/EllipticArcTest.java
@@ -1,9 +1,8 @@
 package br.org.archimedes.ellipticarc.tests;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,103 +13,105 @@ import br.org.archimedes.model.Point;
 
 public class EllipticArcTest {
 	
-	private Point widthPoint1;
-	private Point heightPoint1;
+	private Point topPoint1;
+	private Point rightPoint1;
 	private Point center1;
 	private EllipticArc ellipticArc1;
-	private Point endPoint1;
-	private Point initialPoint1;
+	private Point leftPoint1;
+	private Point downPoint1;
 
 	@Before
 	public void setUp() throws Exception {
 		this.center1 = new Point(0, 0);
-		this.widthPoint1 = new Point(0, 10);
-		this.heightPoint1 = new Point(5, 0);
-		this.initialPoint1 = new Point(0, 10);
-		this.endPoint1 = new Point(5, 0);
-		this.ellipticArc1 = new EllipticArc(center1, widthPoint1, heightPoint1, initialPoint1, endPoint1);
+		this.topPoint1 = new Point(0, 10);
+		this.rightPoint1 = new Point(5, 0);
+		this.leftPoint1 = new Point(-5, 0);
+		this.downPoint1 = new Point(0, -10);
+		this.ellipticArc1 = new EllipticArc(center1, topPoint1, rightPoint1, topPoint1, rightPoint1);
 	}
 	@Test
 	public void shouldEqualsToAnotherEllipseIfAllPointsAreEquals()
 			throws Exception {
-		EllipticArc ellipticArc = new EllipticArc(center1, widthPoint1, heightPoint1, initialPoint1, endPoint1);
+		EllipticArc ellipticArc = new EllipticArc(center1, topPoint1, rightPoint1, topPoint1, rightPoint1);
 
 		assertTrue(ellipticArc1.equals(ellipticArc));
-	}
-	
-
-	
+	}	
 	@Test
 	public void shouldNotAcceptBrokenEllipticArc() throws NullArgumentException {
 		Point endPoint2 = new Point(6,0);
 		boolean trow = false;
 		try{
-			new EllipticArc(center1, widthPoint1, heightPoint1, initialPoint1, endPoint2);
+			new EllipticArc(center1, topPoint1, rightPoint1, topPoint1, endPoint2);
 		}
 		catch( InvalidArgumentException e){
 			trow = true;
 		}
-		assertTrue(trow);
-	
-	}
-	
+		assertTrue(trow);	
+	}	
 	@Test
 	public void shouldNotAcceptBrokenEllipticArc2() throws NullArgumentException {
 		boolean trow = false;
 		try{
-			new EllipticArc(center1, widthPoint1, widthPoint1, initialPoint1, endPoint1);
+			new EllipticArc(center1, topPoint1, topPoint1, topPoint1, rightPoint1);
 		}
 		catch( InvalidArgumentException e){
 			trow = true;
 		}
-		assertTrue(trow);
-	
+		assertTrue(trow);	
 	}
 	@Test
 	public void shouldNotAcceptBrokenEllipticArc3() throws NullArgumentException {
 		boolean trow = false;
 		try{
-			new EllipticArc(center1, center1, heightPoint1, initialPoint1, endPoint1);
+			new EllipticArc(center1, center1, rightPoint1, topPoint1, rightPoint1);
 		}
 		catch( InvalidArgumentException e){
 			trow = true;
 		}
 		assertTrue(trow);
-	
 	}
 	@Test
 	public void shouldNotAcceptBrokenEllipticArc4() throws NullArgumentException {
 		boolean trow = false;
 		try{
-			new EllipticArc(center1, widthPoint1, center1, initialPoint1, endPoint1);
+			new EllipticArc(center1, topPoint1, center1, topPoint1, rightPoint1);
 		}
 		catch( InvalidArgumentException e){
 			trow = true;
 		}
 		assertTrue(trow);
-	
 	}
 	
 	@Test
 	public void shouldCreateEllipseWithGreaterHeightThanWidth()
 			throws Exception {
-		new EllipticArc(center1, heightPoint1, widthPoint1, initialPoint1, endPoint1);
-
+		new EllipticArc(center1, rightPoint1, topPoint1, topPoint1, rightPoint1);
 		assertTrue(true);
 	}
 	
 	@Test
 	public void EllipticArcClone() throws InvalidArgumentException, NullArgumentException{
-		EllipticArc e = new EllipticArc(center1, heightPoint1, widthPoint1, initialPoint1, endPoint1);
+		EllipticArc e = new EllipticArc(center1, rightPoint1, topPoint1, topPoint1, rightPoint1);
 		EllipticArc f = e.clone();
 		assertTrue(e.equals((Object)f));
 		
 	}
-	
-	@After
-	public void tearDown() throws Exception {
+	@Test
+	public void EllipticArcNotContains() throws InvalidArgumentException, NullArgumentException{
+		EllipticArc e = new EllipticArc(center1, topPoint1, rightPoint1, rightPoint1, leftPoint1);
+		assertFalse(e.contains(downPoint1));
+		
 	}
-
-	
-
+	@Test
+	public void EllipticArcContains() throws InvalidArgumentException, NullArgumentException{
+		EllipticArc e = new EllipticArc(center1, topPoint1, rightPoint1, downPoint1, topPoint1);
+		assertTrue(e.contains(rightPoint1));
+		
+	}
+	@Test
+	public void EllipticArcNotContains1() throws InvalidArgumentException, NullArgumentException{
+		EllipticArc e = new EllipticArc(center1, topPoint1, rightPoint1, downPoint1, topPoint1);
+		assertFalse(e.contains(leftPoint1));
+		
+	}
 }
