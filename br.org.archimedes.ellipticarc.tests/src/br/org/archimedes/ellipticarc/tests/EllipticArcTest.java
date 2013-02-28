f022a433c84cab4b7cbdf4ca8948f0319f3b3247
RobertoRodrigues <me@rrodrigues.net>
2013-02-28 15:25:42 -0300

    placing license

--- br.org.archimedes.ellipticarc.tests/src/br/org/archimedes/ellipticarc/tests/EllipticArcTest.java
+++ br.org.archimedes.ellipticarc.tests/src/br/org/archimedes/ellipticarc/tests/EllipticArcTest.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2006, 2009 Hugo Corbucci and others.<br>
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html<br>
+ * <br>
+ * Contributors:<br>
+ * Hugo Corbucci - initial API and implementation<br>
+ * Roberto L. M. Rodrigues, Eduardo Morais and Neuton Jr.<br>
+ * <br>
+ * This file was created on 2013/02/28, 13:06:39, by Roberto L. M. Rodrigues.<br>
+ * It is part of package br.org.archimedes.ellipticArcTest on the br.org.archimedes.ellipticArcTest project.<br>
+ */
+
 package br.org.archimedes.ellipticarc.tests;
 
 import static org.junit.Assert.*;
